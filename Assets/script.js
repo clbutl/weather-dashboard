5c752fa78730485ed67f385db767ac0c23b1e715
Cannon <cannonlbutler@gmail.com>
2022-08-26 01:12:14 -0400

    Finishing Touches to challenge and README

--- Assets/script.js
+++ Assets/script.js
@@ -21,7 +21,6 @@ function getWeather(api) {
   .then(function (response) {
     if (response.status === 200) {
       weatherInfo.classList.remove('hidden')
-      weatherInformation(api)
       return response.json();
     } else {
       cityInputEl.classList.remove('normal-border')
@@ -146,10 +145,8 @@ function weatherInformation(api) {
     currentWind.textContent = data.wind.speed + ' MPH'
     currentHumidity.textContent = data.main.humidity + '%'
     console.log(data)
-    currentUV.textContent = data.uvi
+    currentUV.textContent = "N/A.."
 
-    var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInputEl.value + '&cnt=5&appid=' + APIKeyForecast;
-    futureWeather(forecastAPI)
     futureDates()
   })
 }
@@ -165,28 +162,56 @@ inputForm.addEventListener('submit', function(event){
   }
   cityInputEl.classList.remove('error-border')
   cityInputEl.classList.add('normal-border')
-  var futureCities = []
-  if (futureCities[0]) {
-    futureCities.concat(cityInputEl.value)
+
+
+  if (localStorage.getItem('city1')) {
+    if(localStorage.getItem('city2')) {
+      if(localStorage.getItem('city3')) {
+        if(localStorage.getItem('city4')) {
+          if(localStorage.getItem('city5')) {
+            console.log('storage full')
+          } else {
+            if (cityInputEl.value === localStorage.getItem('city1') || cityInputEl.value === localStorage.getItem('city2') || cityInputEl.value === localStorage.getItem('city3') || cityInputEl.value === localStorage.getItem('city4')) {
+              console.log('duplicate')
+            } else {
+              localStorage.setItem('city5', cityInputEl.value)
+            }
+          } 
+        } else {
+          if (cityInputEl.value === localStorage.getItem('city1') || cityInputEl.value === localStorage.getItem('city2') || cityInputEl.value === localStorage.getItem('city3')) {
+            console.log('duplicate')
+          } else {
+            localStorage.setItem('city4', cityInputEl.value)
+          }
+        } 
+      } else {
+        if (cityInputEl.value === localStorage.getItem('city1') || cityInputEl.value === localStorage.getItem('city2')) {
+          console.log('duplicate')
+        } else {
+          localStorage.setItem('city3', cityInputEl.value)
+        }
+      } 
+    } else {
+      if (cityInputEl.value === localStorage.getItem('city1')) {
+        console.log('duplicate')
+      } else {
+        localStorage.setItem('city2', cityInputEl.value)
+      }
+    } 
   } else {
-    futureCities.push(cityInputEl.value)
+    localStorage.setItem('city1', cityInputEl.value)
   }
 
-  console.log(futureCities)
-
-
-  var localCities = localStorage.getItem('cities')
-
-  if (localCities) {
-    if (!localCities.includes(cityInputEl.value)) {
-      localStorage.setItem('cities', futureCities.push(cityInputEl.value))
-    }
-  } else {
-    localStorage.setItem('cities', cityInputEl.value)
-    console.log(localStorage.getItem('cities'))
-  }
+  
+  recentCities()
+  
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.value + '&appid=' + APIKey;
   getWeather(requestUrl)
+  weatherInformation(requestUrl)
+  
+  var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInputEl.value + '&cnt=5&appid=' + APIKeyForecast;
+  futureWeather(forecastAPI)
+
   // getNames()
 })
 
@@ -202,14 +227,95 @@ function getNames() {
 }
 
 
-function pastCities(name) {
-  var pastCities = document.createElement('li');
-  pastCities.classList.add('past-inputs', 'bg-light', 'p-0');
+function recentCities() {
+  if (localCity1) {
+    var localBtn1 = document.getElementById('city-1-local')
+    localBtn1.textContent = localCity1
+    document.getElementById('city-1').classList.remove('hidden')
+    localBtn1.addEventListener('click', function (){
+      runWeather(localCity1)
+    })
+  } 
 
-  var pastCitiesButton = document.createElement('button');
-  pastCitiesButton.classList.add('border-0', 'bg-light', 'w-100', 'p-2', 'round-border');
-  // pastCitiesButton.textContent = 
-  pastCities.append(pastCitiesButton);
+  if (localCity2) {
+    var localBtn2 = document.getElementById('city-2-local')
+    localBtn2.textContent = localCity2
+    document.getElementById('city-2').classList.remove('hidden')
+    localBtn2.addEventListener('click', function (){
+      runWeather(localCity2)
+    })
+  } 
 
-  cities.append(pastCities)
+  if (localCity3) {
+    var localBtn3 = document.getElementById('city-3-local')
+    localBtn3.textContent = localCity3
+    document.getElementById('city-3').classList.remove('hidden')
+    localBtn3.addEventListener('click', function (){
+      runWeather(localCity3)
+    })
+  } 
+
+  if (localCity4) {
+    var localBtn4 = document.getElementById('city-4-local')
+    localBtn4.textContent = localCity4
+    document.getElementById('city-4').classList.remove('hidden')
+    localBtn4.addEventListener('click', function (){
+      runWeather(localCity4)
+    })
+  } 
+  
+  if (localCity5) {
+    var localBtn5 = document.getElementById('city-5-local')
+    localBtn5.textContent = localCity5
+    document.getElementById('city-5').classList.remove('hidden')
+    localBtn5.addEventListener('click', function (){
+      runWeather(localCity5)
+    })
+  } 
+
 }
+
+function runWeather(local) {
+
+  
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + local + '&appid=' + APIKey;
+  getWeather(requestUrl)
+  weatherInformation2(requestUrl, local)
+  
+  var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=' + local + '&cnt=5&appid=' + APIKeyForecast;
+  futureWeather(forecastAPI)
+}
+
+function weatherInformation2(api, local) {
+  fetch(api)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
+    var icon = data.weather[0].icon
+    var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+    fetch(iconUrl)
+    .then(function () {
+      currentCity.textContent = local + '(' + moment().format('M/D/YYYY') + ')'
+      weatherIcon.setAttribute('src', 'http://openweathermap.org/img/w/' + icon + '.png')
+    })  
+    currentTemp.textContent = Math.round((1.8*((data.main.temp)-273) + 32)*10)/10 + '°F'
+    currentWind.textContent = data.wind.speed + ' MPH'
+    currentHumidity.textContent = data.main.humidity + '%'
+    console.log(data)
+    currentUV.textContent = "N/A.."
+
+    futureDates()
+  })
+}
+
+
+
+var localCity1 = localStorage.getItem('city1')
+var localCity2 = localStorage.getItem('city2')
+var localCity3 = localStorage.getItem('city3')
+var localCity4 = localStorage.getItem('city4')
+var localCity5 = localStorage.getItem('city5')
+
+recentCities()
---
+++ README.md
@@ -0,0 +1,26 @@
+# weather-dashboard
+
+## Description
+
+This week's challenge was about creating a a weather tracker. The application needed to display the current weather for an inputted city, along with the 5 day forcast and previous city inputs.
+
+# Challenges
+
+I had a few challenges while going through this challenge, these include using local storage with the previous cities and using the API correctly
+
+## Using Local Storage
+
+With the past few activities one of the main challenges that I face is using local storage. Even though I've been using it for a few weeks I still had trouble making it completely correctly.  However I still got a working part done while using the local storage.
+
+This is an example of the Local Storage Inputs
+![Local Storage - Image Here](./Assets/Local%20Storage%20Capture.PNG) 
+
+## Using the API
+
+This was my first activity using an API so it was tricky getting used to it.  However, after digging around a little bit and looking at previous activities I was able to figure it out. One main issue was that I was not able to get the UVI Data from the weather API, I was talking to my classmates and we believe that it was taken out and put in a premium version of the API.
+
+# Conclusion
+
+All in all this challenge really helped me with going out of my confort zone while using APIs. I'm very happy with how this turned out and I hope I continue to understand these amazing concepts!
+
+Deployed Application: https://clbutl.github.io/weather-dashboard/
